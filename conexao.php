37f98207ab0c92fd406468e6516e7764f4987c5e
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-10-11 11:45:41 -0300

    Create conexao com o bando de dados

--- conexao.php
+++ conexao.php
@@ -4,12 +4,22 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$dbname = "b";
-$host = "localhost";
+$dbname = "banco_dados-parcela.sql";
+$port = 3306;
 
 
+try {
+    //Conexão com a porta
+    //$conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
 
+    //Conexão sem a porta
+    $conn = new PDO("mysql:host=$host;dbname=" . $dbname, $user, $pass);
+    //echo "Conexão com o bando de dados realizada com sucesso!";
+}catch(PDOException $err){
+    echo "Erro: Conexão com o banco de dados não foi realizada! Erro gerado " . $err->getMessage();
+}
 
+//Fim da conexão com o bando de dados utilizando PDO
 
 
 
